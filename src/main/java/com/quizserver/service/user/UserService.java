6a116bfc4e2daa0be0d2110ed2979e989d7a1156
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-02-20 21:16:44 -0500

    Creación de la API de registro sign-up con sus respectivos endpoints

--- src/main/java/com/quizserver/service/user/UserService.java
+++ src/main/java/com/quizserver/service/user/UserService.java
@@ -1,4 +1,8 @@
 package com.quizserver.service.user;
 
+import com.quizserver.entities.User;
+
 public interface UserService {
+    User createUser(User user);
+    Boolean hasUserWithEmail(String email);
 }
