55c8c6da07e74a0caeea2840ce117e965c225580
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-02-25 16:59:31 -0500

    Creación de la API para añadir una pregunta al examen

--- src/main/java/com/quizserver/controller/TestController.java
+++ src/main/java/com/quizserver/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.quizserver.controller;
 
+import com.quizserver.dto.QuestionDTO;
 import com.quizserver.dto.TestDTO;
 import com.quizserver.service.test.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,15 @@ public class TestController {
     public ResponseEntity<?> createTest(@RequestBody TestDTO dto) {
         try {
             return new ResponseEntity<>(testService.createTest(dto), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/question")
+    public ResponseEntity<?> addQuestionInTest(@RequestBody QuestionDTO dto) {
+        try {
+            return new ResponseEntity<>(testService.addQuestionInTest(dto), HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
